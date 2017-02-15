f3d7e53e58d364254590a947ef8f25a86662ba0d
Austin Rogers <contactALRogers@gmail.com>
2017-02-15 10:13:34 -0800

    done with TODOs

--- starter-code/server.js
+++ starter-code/server.js
@@ -1,20 +1,27 @@
 'use strict';
 
-// TODO: Initialize your project using NPM to create and populate a package.json file
+// DONE: Initialize your project using NPM to create and populate a package.json file
 
-// TODO: Require the Express package that you installed via NPM, and instantiate the app
+// DONE: Require the Express package that you installed via NPM, and instantiate the app
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
+const express = require('express');
+
+const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-// TODO: Include all of the static resources as an argument to app.use()
+// DONE: Include all of the static resources as an argument to app.use()
+app.use(express.static('./public'));
 
 app.get('*', function(request, response) {
-  // TODO: Using the response object, send the index.html file back to the user
+  // DONE: Using the response object, send the index.html file back to the user
+  response.sendFile('index.html', {root: './public'});
 });
 
 // TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
 
+
+// TODO: Log to the console a message that lets you know which port your server has started on
 app.listen(PORT, function() {
-  // TODO: Log to the console a message that lets you know which port your server has started on
+  console.log('Our amazing app is being served at localhost:3000')
 });
